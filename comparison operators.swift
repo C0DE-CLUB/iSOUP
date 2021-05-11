59f8fe063f74e2f96ee5553980e73166267d8309
TyphonMedia <75556422+TyphonMedia@users.noreply.github.com>
2021-05-11 14:03:47 -0700

    Update comparison operators.swift

--- comparison operators.swift
+++ comparison operators.swift
@@ -12,4 +12,4 @@ You can compare one value to another using comparison operators
 7 > 2 // returns true because 7 is greater than 2
 2 < 7 // returns true because 2 is less than 7 
 3 >= 3 // returns true because 3 is greater than or equal to 3
-7  <= 2 returns false because 7 is not less than or equal to 2
+7  <= 2 //returns false because 7 is not less than or equal to 2
