85baa959c1bca9915c86ddaa854ba9b3feda79eb
TyphonMedia <75556422+TyphonMedia@users.noreply.github.com>
2021-05-11 19:29:24 -0700

    Create Understanding Enumerations.swift

---
+++ Understanding Enumerations.swift
@@ -0,0 +1,30 @@
+For more information on Enumeration, visit https://docs.swift.org/swift-book/LanguageGuide/Enumerations.html
+
+enum Month { // The keyword for enumeration is enum, for this example we will create an enum and name it Month.
+    case January
+    case February // Every month in front of case is a definded member value
+    case March
+    case April
+    case May
+    case June
+    case July
+    case August
+    case September
+    case October
+    case November
+    case December
+    
+    var season: String {
+        switch self {
+            case Month.December, Month.January, Month.February:
+                return "Winter"
+            case Month.March, Month.April, Month.May:
+                return "Spring"
+            case Month.June, Month.July, Month.August:
+                return "Summer"
+            case Month.September, Month.October, Month.November:
+                return "Autumn"
+        }
+    }
+}
+print(Month.July.season) // You can change the month for the print statement and the program will print out the appropiate season for that month
