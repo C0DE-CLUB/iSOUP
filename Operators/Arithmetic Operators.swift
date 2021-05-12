4c4b064646e8b0fc432d3603356e7c1a4ef4327e
TyphonMedia <75556422+TyphonMedia@users.noreply.github.com>
2021-05-12 13:46:04 -0700

    Delete Operators directory

--- Operators/Arithmetic Operators.swift
+++
@@ -1,9 +0,0 @@
-+ // A plus sign is used to represent Addition
-- // A hyphen is used to represent Subtraction
-* // An asterisk is used to represent Multiplication
-/ // A front slash is used to represent Division
-
-let sum = 44 + 44 // The sum is 88
-let result = 21 - sum // The result here is - 67
-let total = result * 3 // The total here is - 201
-let divide = total/7 // Note that divided by 7 returns -28 instead of -28.714, as both numbers are integers
--- Operators/Order of Operations.md
+++
@@ -1,1 +0,0 @@
-![Order of Operations](https://i.imgur.com/3OXck6b.png)
